ba25223d1ff2c0ac4772585133657b29f483e982
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2017-03-12 12:21:38 -0300

    Encoding categorical data

--- section2/data_preprocessing_template.R
+++ section2/data_preprocessing_template.R
@@ -1,2 +1,21 @@
 # Data Preprocessing
+
+# Importing the dataset
 dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         c('Spain', 'France', 'Germany'),
+                         c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           c('No', 'Yes'),
+                           c(0, 1))
--- section2/data_preprocessing_template.py
+++ section2/data_preprocessing_template.py
@@ -4,7 +4,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
 
+
+# Importing data
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1:].values
+                
+# Taking care of missing data
+imputer = Imputer()
+imputer = imputer.fit(X[:, 1:])
+X[:, 1:] = imputer.transform(X[:, 1:])
+
+# Encoding categorical data
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
