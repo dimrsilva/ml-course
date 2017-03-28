c106b97ce0f97f1699d0ccaa938765e0358bf662
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2017-03-28 20:42:13 -0300

    Multiple Linear Regression

---
+++ section5/multiple_linear_regression.py
@@ -0,0 +1,40 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.formula.api as sm
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+# Importing data
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1:].values
+
+# Encoding categorical data
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training Set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test Set results
+y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
